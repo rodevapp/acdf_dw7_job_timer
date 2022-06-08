affddbfd71f92e801cfdd32ef6c3a9705055a276
Rodrigo Flores <rodevapp@gmail.com>
2022-06-08 18:02:10 +0100

    initial settings modular

---
+++ lib/app/app_module.dart
@@ -0,0 +1,19 @@
+import 'dart:js';
+
+import 'package:acdf_dw7_job_timer/app/modules/splash/splash_page.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+class AppModule extends Module {
+  @override
+  // TODO: implement binds
+  List<Bind<Object>> get binds => [];
+
+  @override
+  // TODO: implement routes
+  List<ModularRoute> get routes => [
+        ChildRoute(
+          "/",
+          child: (context, args) => const SplashPage(),
+        )
+      ];
+}
---
+++ lib/app/app_widget.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      title: "Job Timer",
+      routeInformationParser: Modular.routeInformationParser,
+      routerDelegate: Modular.routerDelegate,
+    );
+  }
+}
